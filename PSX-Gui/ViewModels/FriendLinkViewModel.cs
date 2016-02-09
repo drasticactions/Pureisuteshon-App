ef8ae961a1333e8afe408d7a46b613599748e667
Tim Miller <innerlogic4321@gmail.com>
2016-02-09 16:19:07 -0500

    Add FriendLinkPage.xaml

---
+++ PSX-Gui/ViewModels/FriendLinkViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Chat;
+using Windows.ApplicationModel.Email;
+using Windows.ApplicationModel.Resources;
+using Newtonsoft.Json;
+using PlayStation.Managers;
+using PlayStation_App.Models.Response;
+using PlayStation_App.Tools.Debug;
+using PlayStation_App.Tools.Helpers;
+using PlayStation_Gui.Views;
+using Template10.Mvvm;
+
+namespace PlayStation_Gui.ViewModels
+{
+    public class FriendLinkViewModel : ViewModelBase
+    {
+        private readonly FriendManager _friendManager = new FriendManager();
+        private readonly ResourceLoader _loader = new ResourceLoader();
+
+        private bool _isLoading;
+
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                Set(ref _isLoading, value);
+            }
+        }
+
+        public async Task<string> CreateFriendLink()
+        {
+            var result = await _friendManager.GetFriendLink(Shell.Instance.ViewModel.CurrentTokens);
+            await AccountAuthHelpers.UpdateTokens(Shell.Instance.ViewModel.CurrentUser, result);
+            var resultCheck = await ResultChecker.CheckSuccess(result);
+            if (!resultCheck)
+            {
+                return string.Empty;
+            }
+            var tokenEntity = JsonConvert.DeserializeObject<TokenResponse>(result.ResultJson);
+            return tokenEntity.Token;
+        }
+
+        public async Task SendFriendLinkViaSms()
+        {
+            IsLoading = true;
+            var result = await ChatMessageManager.GetTransportsAsync();
+            if (!result.Any())
+            {
+                IsLoading = false;
+                await ResultChecker.SendMessageDialogAsync(_loader.GetString("NoSMS/Text"), false);
+                return;
+            }
+            var link = await CreateFriendLink();
+
+            if (!string.IsNullOrEmpty(link))
+            {
+                var chat = new ChatMessage
+                {
+                    Subject = _loader.GetString("FriendRequestBody/Text"),
+                    Body = string.Format("{0} {1}", _loader.GetString("FriendRequestBody/Text"), link)
+                };
+                await ChatMessageManager.ShowComposeSmsMessageAsync(chat);
+            }
+            IsLoading = false;
+        }
+
+        public async Task SendFriendLinkViaEmail()
+        {
+            IsLoading = true;
+            var link = await CreateFriendLink();
+            if (!string.IsNullOrEmpty(link))
+            {
+                var em = new EmailMessage
+                {
+                    Subject = _loader.GetString("FriendRequestBody/Text"),
+                    Body = string.Format("{0} {1}", _loader.GetString("FriendRequestBody/Text"), link)
+                };
+                await EmailManager.ShowComposeNewEmailAsync(em);
+            }
+            IsLoading = false;
+        }
+    }
+}
---
+++ PSX-Gui/Views/FriendLinkPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using PlayStation_Gui.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PlayStation_Gui.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FriendLinkPage : Page, IDisposable
+    {
+        public FriendLinkPage()
+        {
+            this.InitializeComponent();
+            DataTransferManager.GetForCurrentView().DataRequested += OnDataRequested;
+        }
+
+        private string _link = string.Empty;
+
+        private void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+            args.Request.Data.SetText(
+                $"{loader.GetString("SendMeFriendRequest/Text")} {_link} {Environment.NewLine} {loader.GetString("SentFromPlayStationApp/Text")}");
+            args.Request.Data.Properties.Title = loader.GetString("InviteFriendsToPsn/Text");
+        }
+
+        public FriendLinkViewModel ViewModel => this.DataContext as FriendLinkViewModel;
+
+        private async void ShareInvite_OnClick(object sender, RoutedEventArgs e)
+        {
+            ViewModel.IsLoading = true;
+            var _link = await ViewModel.CreateFriendLink();
+            ViewModel.IsLoading = false;
+            if (string.IsNullOrEmpty(_link))
+            {
+                return;
+            }
+            DataTransferManager.ShowShareUI();
+        }
+
+        public void Dispose()
+        {
+            DataTransferManager.GetForCurrentView().DataRequested -= OnDataRequested;
+        }
+    }
+}
